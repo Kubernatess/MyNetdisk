26752db81e708825c5c3df4e02dda2005d53e6f8
MyCloudStack <865096492@qq.com>
2018-12-11 16:01:06 +0800

    success

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,14 +1,19 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1 , user-scalable=no">
-
-
 <title>index</title>
-<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="css/bootstrap-index.css" rel="stylesheet" />
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/bootstrap-index.css" />
+<script>
+var xhr=new XMLHttpRequest();
+
+</script>
 </head>
 <body>
 <!--导航-->
@@ -51,7 +56,13 @@
 <section id="guide">
 <div class="container">
 <div class="row">
-<span class="col-md-2"><form method="post" action=""><label for="upload" class="btn btn-primary btn-lg">上传</label><input type="file" name="upload" id="upload" multiple style="display:none" /></form></span>
+<span class="col-md-2">
+	<form method="post" action="${pageContext.request.contextPath}/upload" enctype="multipart/form-data">
+	<label for="upload" class="btn btn-primary btn-lg">上传</label>
+	<input type="file" name="myfile" id="upload" multiple style="display:none" />
+	<input type="submit">
+	</form>
+</span>
 <span class="col-md-2"><button class="btn btn-info btn-lg">下载</button></span>
 <span class="col-md-4 navbar-right"><form class="form-inline">
 <div class="input-group">
---
+++ src/UploadServlet.java
@@ -0,0 +1,86 @@
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@WebServlet("/upload")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//创建磁盘文件工厂类
+		DiskFileItemFactory factory=new DiskFileItemFactory();
+		//创建核心解析对象
+		ServletFileUpload upload=new ServletFileUpload(factory);
+		//解析request
+		try {
+			List<FileItem> list=upload.parseRequest(request);
+			//循环遍历
+			for(FileItem fileItem:list){
+				//isFormField()返回true表示普通文本项,返回false表示文件上传项
+				if(fileItem.isFormField()){
+					String name=fileItem.getFieldName();
+					String value=fileItem.getString();
+				}else{
+					//文件上传项
+					String filename=fileItem.getName();
+					//获取文件的输入流
+					InputStream in=fileItem.getInputStream();
+					//向某个文件中写入
+					//向WebRoot/upload目录写入
+					String path=getServletContext().getRealPath("/directory");
+					
+					//获取输出流
+					OutputStream os=new FileOutputStream(path+"/"+filename);
+					int len=0;
+					byte b[]=new byte[1024];
+					while((len=in.read(b))!=-1){
+						os.write(b,0,len);
+					}
+					in.close();
+					os.close();
+				}
+			}
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
